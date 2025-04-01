35e7474245ade027d2b49b7384f08fa695b0cebe
webandrs <d3vv.proo@gmail.com>
2025-04-01 12:29:41 +0700

    [FE]: Created footer. Desktop and mobile with dropdowns.

--- layouts/src/js/common/common.js
+++ layouts/src/js/common/common.js
@@ -1,3 +1,11 @@
-document.addEventListener( 'DOMContentLoaded', () => {
-	'use strict'
-} )
+import { WINDOW_WIDTH_MD } from "./global"
+
+document.addEventListener('DOMContentLoaded', () => {
+	document.addEventListener('click', e => {
+		const target = e.target
+		if (target.closest('.col-title') && window.innerWidth < WINDOW_WIDTH_MD) {
+			const footerCol = e.target.closest('.footer-col')
+			if (footerCol) footerCol.classList.toggle('active')
+		}
+	})
+})
---
+++ layouts/src/js/common/global.js
@@ -0,0 +1,1 @@
+export const WINDOW_WIDTH_MD = 768
