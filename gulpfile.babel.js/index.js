171d99bc942f2389695034bf8d1321b331e4af99
webandrs <d3vv.proo@gmail.com>
2025-04-21 23:38:16 +0700

    [FIX]: Fixed aggressive css minification when build.

--- gulpfile.babel.js/index.js
+++ gulpfile.babel.js/index.js
@@ -7,7 +7,6 @@ import notify from 'gulp-notify'
 import autoprefixer from 'gulp-autoprefixer'
 import csso from 'gulp-csso'
 import rename from 'gulp-rename'
-import groupCssMediaQueries	from 'gulp-group-css-media-queries'
 import dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 import webpCss from 'gulp-webp-css'
@@ -46,9 +45,10 @@ const scss = () => {
 		.pipe( sass() )
 		.pipe( webpCss() )
 		.pipe( autoprefixer() )
-		.pipe( gulpIf( app.isProd, groupCssMediaQueries() ) )
 		.pipe( rename( { suffix: '.min' } ) )
-		.pipe( gulpIf( app.isProd, csso() ) )
+		.pipe( gulpIf( app.isProd, csso({
+			restructure: false,
+		}) ) )
 		.pipe( gulp.dest( path.scss.dest, { sourcemaps: app.isDev } ) )
 		.pipe( browserSync.stream() )
 }
