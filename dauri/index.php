dbc6cd3bb3dff83fdec19ccd327405aa8b10337f
ns <NSukonny@gmail.com>
2025-06-04 23:12:20 +0300

    added single page

--- dauri/index.php
+++ dauri/index.php
@@ -9,8 +9,6 @@ get_header();
 
 if ( is_front_page() || is_home() ) {
 	get_template_part( 'components/front_page/front_page' );
-} else {
-
 }
 
 get_footer();
--- dauri/single.php
+++ dauri/single.php
@@ -1,30 +1,28 @@
 <?php
-
+/**
+ * The template for displaying all single posts.
+ *
+ * @package DAURI
+ */
 global $post;
 
 get_header();
-?>
-    <div class="wrapper">
-        <?php
-        get_template_part(
-            'components/banners/banners',
-            '',
-            array(
-                'template' => 'horizontal',
-                'banner_field' => 'horizontal_banner',
-            )
-        );
-        ?>
-        <div class="main_wrapper">
-            <?php
-            get_template_part('components/left-menu/left-menu');
 
-            get_template_part('single/single-' . $post->post_type);
-            ?>
-        </div>
+$wc_product = wc_get_product( $post->ID );
+if ( empty( $wc_product ) ) {
+	wp_safe_redirect( home_url( '/404' ) );
+	exit;
+}
 
-    </div>
-<?php
-add_video_view($post);
+get_template_part( 'template-parts/card/card', null, array( 'product' => $wc_product ) );
+get_template_part( 'template-parts/card/perks' );
+get_template_part( 'template-parts/card/tabs', null, array( 'product' => $wc_product ) );
+/**
+ * @@include( 'template-parts/sections/card/card/card.html' )
+ * @@include( 'template-parts/sections/card/perks/perks.html' )
+ * @@include( 'template-parts/sections/card/tabs/tabs.html' )
+ * @@include( 'template-parts/sections/home/slides-half/slides-half.html', {'class': 'type-a', 'title': 'ПОДОБРАЛИ ДЛЯ', 'span': 'вас', 'link': 'Перейти в каталог'} )
+ * @@include( 'template-parts/sections/card/bottom-cta/bottom-cta.html' )
+ */
 
 get_footer();
